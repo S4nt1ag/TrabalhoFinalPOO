b0c198911cdb91870b5b266b99098750446b33ac
isabeldefreitas <127349333+isabeldefreitas@users.noreply.github.com>
2023-04-18 17:12:20 -0300

    atualização conta

--- src/contas/ContaCorrente.java
+++ src/contas/ContaCorrente.java
@@ -2,10 +2,40 @@ package contas;
 
 public class ContaCorrente extends Conta{
 
+	private double saldo;
+
+
 	public ContaCorrente(String nome, String cpf, Integer senha, String email, Integer telefone, String endereco) {
 		super(nome, cpf, senha, email, telefone, endereco);
 		
 	}
 	
-	
+		public void sacar(double valor) {
+				
+				if (valor <= 0) {
+					System.out.println("Valor inválido para saque.");
+					
+				} else if (valor > this.saldo) {
+					System.out.println("Saldo insuficiente.");
+					
+				} else {
+					this.saldo -= valor;
+					System.out.println("Saque realizado com sucesso!");
+				}
+			}
+			
+			
+			public void depositar(double valor) {
+				
+				if (valor <= 0) {
+					System.out.println("Valor inválido para depósito.");
+					
+				} else {
+					this.saldo += valor;
+					System.out.println("Depósito realizado com sucesso!");
+				}
+			}
+			
+			
+
 }
--- src/contas/ContaPoupanca.java
+++ src/contas/ContaPoupanca.java
@@ -2,9 +2,33 @@ package contas;
 
 public class ContaPoupanca extends Conta {
 
+	private double saldo;
+
 	public ContaPoupanca(String nome, String cpf, Integer senha, String email, Integer telefone, String endereco) {
 		super(nome, cpf, senha, email, telefone, endereco);
 		
 	}
+	
+	public void sacar(double valor) {
+
+		if (valor <= 0) {
+			System.out.println("Valor inválido para saque.");
+
+		} else if (valor > this.saldo) {
+			System.out.println("Saldo insuficiente.");
+
+		} else {
+			this.saldo -= valor;
+			System.out.println("Saque realizado com sucesso!");
+		}
+	}
+
+	public void depositar(double valor) {
+
+		this.saldo += valor;
+		System.out.println("Depósito realizado com sucesso!");
+
+	}
+
 
 }
